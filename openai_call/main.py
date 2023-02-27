33eb8b57a6db549150bca65e4cba8b0e166dcfba
MaxFifield <mfifield@deloitte.com.au>
2023-02-27 15:19:37 +1000

    Update function app for int with Cog Search

--- openai_call/main.py
+++ openai_call/main.py
@@ -1,8 +1,10 @@
 import logging
 import azure.functions as func
 
+
 from . import search
 from . import chatgpt
+from search import AzureSearchClient as ASC
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Starting Azure functions call for ChatGPT')
@@ -24,15 +26,31 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             """No suitable question supplied. Please provide the input in the form; {"question": <input>}""",
             status_code=406
         )
+    
     # Log the question
     logging.info(f"User question is: {question}")
     
     # Mock up the response from search. TODO: Replace with correct functionality
-    search_response = search.simulate_search_response(question)
+    #search_response = search.simulate_search_response(question)
+
+    # Initialize Azure Search Client
+    search_client = ASC("https://searchisnow.search.windows.net", "document-index")
+
+    # Refine search request with Azure Search Config
+    srequest = search_client.refine_search(question)
+    logging.info(f"Refined search request paramters are: {srequest}")
+
+    # Query Azure Search
+    search_response = search_client.query_search(srequest)
+
     logging.info(f"Search response is: {search_response}")
-    
+
+    # Add logic to trim response if it is too long
+    cleaned_response = search_client.prep_results_chatgpt(search_response, 300)
+    logging.info(f"Cleaned search response is: {cleaned_response}")
+
     # Generate chatGPT prompt
-    prompt = chatgpt.generate_chatgpt_prompt(question, search_response)
+    prompt = chatgpt.generate_chatgpt_prompt(question, cleaned_response)
     logging.info(f"ChatGPT prompt is: {prompt}")
     
     # Query chatGPT
@@ -44,6 +62,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         )
     
     # If everything is successful return chatGPT response
+    logging.info("Chat GPT Function call successful")
     return func.HttpResponse(
         chatgpt_response,
         status_code=200
